ac660cd43b7c3fe5a5bd676cfba2150dda10d966
RakuLomis <rakulomis@foxmail.com>
2025-03-20 19:06:03 +0800

    Add truncating function.

--- utils/dataframe_tools.py
+++ utils/dataframe_tools.py
@@ -22,29 +22,6 @@ def get_file_path(dir_path: str, prefix: Optional[str] = None, postfix: Optional
     file_names = [] 
     if os.path.exists(dir_path): 
         if os.path.isdir(dir_path): 
-    #         for root, _, files in tqdm(os.walk(dir_path), "get_file_path: "): 
-    #             for file_name in files: 
-    #             # Check prefix condition
-    #                 prefix_ok = (prefix is None) or file_name.startswith(prefix)
-    #                 # Check postfix condition
-    #                 postfix_ok = (postfix is None) or file_name.endswith(postfix)
-
-    #                 if prefix_ok and postfix_ok:
-    #                     # Build full path
-    #                     full_path = os.path.join(root, file_name)
-    #                     target_paths.append(full_path)
-
-    #                 # Process base name 
-    #                 if postfix and file_name.endswith(postfix):
-    #                     base_name = file_name[:-len(postfix)] 
-    #                 if postfix and not file_name.endswith(postfix): 
-    #                     continue 
-    #                 else: # no postfix, record file name
-    #                     base_name = file_name
-    #                 file_names.append(base_name)
-    # else: 
-    #     print("Invalid directory path") 
-    # return target_paths, file_names 
             with os.scandir(dir_path) as entries: 
                 for entry in tqdm(entries, "get_file_path: "):
                     if entry.is_file():
@@ -110,12 +87,55 @@ def output_csv_in_fold(df: pd.DataFrame, fold_path: str, csv_name: str, index: O
 def padding_features(df: pd.DataFrame, padding_value='ffff'): 
     return df.fillna(padding_value) 
 
-def truncating_features(df: pd.DataFrame): 
-    mask_df = df.notnull() 
 
+def get_not_nan_pos(mask_df: pd.DataFrame): 
+    dict_true = {} 
+    for col_num in range(mask_df.shape[1]): 
+        list_true_indices = list(mask_df[mask_df.iloc[:, col_num]].index) 
+        dict_true[col_num] = list_true_indices 
+    return dict_true 
+
+def to_block_continuous(dict_true: dict): 
+    dict_block = {
+        'block': [], 
+        'columns': [], 
+        'rows': [] 
+    } 
+
+    block_flag = 0 
+    last_key = 0 
+    last_value = dict_true[last_key] 
+    list_col = []
+    for key, value in dict_true.items(): 
+        if key == last_key: # init 
+            dict_block['block'].append(block_flag) 
+            list_col.append(key) 
+            dict_block['columns'].append(list_col.copy()) 
+            dict_block['rows'].append(dict_true[key]) 
+        else: 
+            if value == last_value: 
+                dict_block['columns'][block_flag].append(key) 
+            if value != last_value: 
+                block_flag += 1 
+                dict_block['block'].append(block_flag) 
+                list_col.clear() 
+                list_col.append(key) 
+                dict_block['columns'].append(list_col.copy()) 
+                dict_block['rows'].append(dict_true[key]) 
+                last_key = key 
+                last_value =value 
+    return dict_block 
+
+def truncating_features_continuous(df: pd.DataFrame): 
+    mask_df = df.notnull() 
+    dict_true = get_not_nan_pos(mask_df) 
+    dict_block_continuous = to_block_continuous(dict_true) 
+    return dict_block_continuous 
+
+def truncating_features(df: pd.DataFrame): 
     pass 
 
-def padding_or_truncating(df: pd.DataFrame, pon: bool): 
+def padding_or_truncating(df: pd.DataFrame, pon: bool, continuous_truncating=True): 
     """
     Padding the NaN values or truncating the dataframe into various blocks. 
 
