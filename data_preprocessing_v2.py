3e93ae7608b122f10b64310cb4a9198c8ae1bf91
RakuLomis <rakulomis@foxmail.com>
2025-06-24 16:52:56 +0800

    Complete the data processing, now the path and mode can be assigned by terminal order.

---
+++ data_preprocessing_v2.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import os 
+from utils.dataframe_tools import filter_out_nan 
+from utils.dataframe_tools import to_integer_code
+from utils.dataframe_tools import get_file_path 
+from utils.dataframe_tools import output_csv_in_fold 
+from utils.dataframe_tools import padding_or_truncating
+import argparse 
+
+CONTINUOUS_BLOCK = 'continuous' 
+DISCRETE_BLOCK = 'discrete'
+
+# current_path = os.path.dirname(os.path.abspath(__file__))
+# directory_path = os.path.join(current_path, 'Data', 'Test') 
+
+def main(): 
+    parser = argparse.ArgumentParser(description="Truncate the CSV into different Field Blocks. ") 
+    parser.add_argument(
+        '-d', '--directory', 
+        type=str, 
+        required=True, 
+        help="The path to the directory containing original csv files. "
+    )
+    parser.add_argument(
+        '-m', '--mode', 
+        type=str, 
+        default=DISCRETE_BLOCK, 
+        choices=[DISCRETE_BLOCK, CONTINUOUS_BLOCK], 
+        help="The type of the field block to generate. Defaults to 'discrete'."
+    )
+    args = parser.parse_args()
+    directory_path = args.directory
+    block_type = args.mode 
+    print(f"Running in '{block_type}' mode.") 
+    csv_path_list, csv_name_list = get_file_path(directory_path, postfix='.csv') 
+    if csv_path_list is not None: 
+        for csv_path, csv_name in zip(csv_path_list, csv_name_list): 
+            df = pd.read_csv(csv_path) 
+
+            list_df_block = padding_or_truncating(df, False, block_type) 
+            for block_num in range(len(list_df_block)): 
+                output_csv_in_fold(list_df_block[block_num], os.path.join(directory_path, csv_name, block_type), f'{block_num}' + '.csv') 
+            # df_fill_nan = df.fillna('ffff') 
+            # output_csv_in_fold(df_fill_nan, os.path.join(directory_path, 'fill_nan'), 'fill_nan_' + csv_name + '.csv') 
---
+++ get_layer_fields_v2.py
@@ -0,0 +1,61 @@
+import pyshark
+import os
+import pandas as pd 
+from tqdm import tqdm 
+from utils.pcap_tools import get_fields_over_layers 
+from utils.pcap_tools import get_pcap_path 
+from utils.pcap_tools import get_reasemmble_info 
+from utils.dataframe_tools import get_file_path 
+from utils.dataframe_tools import filter_out_nan
+import argparse
+
+""" 
+Get fields hexvalues into two dataframes (fields_values, reassemble information) and merge them into one. 
+Pay attention to the index of packet in python and wireshark. 
+The index in pyshark starts with 0, and in wireshark (including exported .xml) starts from 1. 
+python get_layer_field_v2.py -d "directory_path"
+"""
+
+# current_path = os.path.dirname(os.path.abspath(__file__))
+# directory_path = os.path.join(current_path, 'Data', 'Test') 
+
+def main(): 
+    parser = argparse.ArgumentParser(description="Process pcap files in a specified directory to generate CSVs.")
+    # Add the arguments
+    parser.add_argument(
+        '-d', '--directory', # short term and long term
+        type=str, 
+        required=True, 
+        help="The path to the directory containing pcap files." 
+    ) 
+    args = parser.parse_args() 
+    directory_path = args.directory 
+    print(f"Starting to process pcap files in: {directory_path}") 
+    pcap_path_list, file_name_list = get_pcap_path(directory_path) 
+    # pcap_path_list, file_name_list = get_file_path(dir_path=directory_path, postfix=['pcap, pcapng'])
+    if pcap_path_list is not None: 
+        for pcap_path, file_name in zip(pcap_path_list, file_name_list): 
+            pcap_file = pyshark.FileCapture(pcap_path) 
+            list_fields = get_fields_over_layers(pcap_file) 
+            dict_reassemble = get_reasemmble_info(pcap_file) 
+            df_reassemble = pd.DataFrame({
+                "frame_num": list(dict_reassemble.keys()), 
+                "reassembled_segments": list(dict_reassemble.values()) 
+            }) 
+            # df_reassemble.to_csv(os.path.join(directory_path, 'reassemble_' + file_name + '.csv'), index=False) 
+            df_fields = pd.DataFrame(list_fields) # frame_num
+            # df_fields['index'] = range(1, len(df_reassemble) + 1) 
+            df_merge_tls = pd.merge(df_fields, df_reassemble, on=["frame_num"], how="outer") 
+            print(f'original shape: ${df_merge_tls.shape}')
+            # df_fields.to_csv(os.path.join(directory_path, file_name + '.csv'), index=False) 
+            df_merge_tls = filter_out_nan(df_merge_tls) 
+            # all_nan_cols = df_merge_tls.columns[df_merge_tls.isna().all()] 
+            # df_merge_tls = df_merge_tls.drop(columns=all_nan_cols)
+            print(f'fill out NaN shape: ${df_merge_tls.shape}') 
+            df_merge_tls.to_csv(os.path.join(directory_path, 'merge_' + file_name + '.csv'), index=False)
+            pcap_file.close() 
+    else:
+        print("No pcap files found in the specified directory.")
+
+if __name__ == '__main__': 
+    main()
