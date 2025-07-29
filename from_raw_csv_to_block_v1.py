762ae1da11553b9bcf694948e2fd47ae4a5e7c28
RakuLomis <rakulomis@foxmail.com>
2025-07-29 17:51:32 +0800

    A completed preprocessing pipeline is needed.

---
+++ from_raw_csv_to_block_v1.py
@@ -0,0 +1,16 @@
+import os 
+from utils.pcap_tools import pcap_to_csv 
+from tqdm import tqdm 
+from utils.dataframe_tools import merge_csvs_with_different_columns 
+from utils.dataframe_tools import label_and_merge_csvs
+
+
+raw_csv_directory_path = os.path.join('..', 'TrafficData', 'dataset_29_d1_csv') 
+csv_directory_merged = os.path.join('..', 'TrafficData', 'dataset_29_d1_csv_merged')
+csv_directory_merged_completed = os.path.join(csv_directory_merged, 'completeness')
+completed_csv_path = os.path.join(csv_directory_merged_completed, 'dataset_29_completed_label.csv') 
+
+# 将pcap生成的csv文件按照类别合并
+label_and_merge_csvs(raw_csv_directory_path, csv_directory_merged, need_label=True) 
+# 将所有csv文件合并成一个总的csv
+merge_csvs_with_different_columns(csv_directory_merged, completed_csv_path, postfix='_label.csv') 
